3d60bf86d3f40c6a5048d53449a7c2935388f240
Blessing <bjmbalaka@gmail.com>
2026-04-21 01:23:42 +0200

    API

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectManagementAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
